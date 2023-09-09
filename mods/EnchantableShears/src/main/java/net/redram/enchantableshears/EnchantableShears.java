61793e32fc30841a894d7fc871053e1a0a316b2b
RedRam <your@email.com>
2023-09-09 10:09:03 -0700

    Move tested version info

--- mods/EnchantableShears/src/main/java/net/redram/enchantableshears/EnchantableShears.java
+++ mods/EnchantableShears/src/main/java/net/redram/enchantableshears/EnchantableShears.java
@@ -5,8 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// tested and works with java 17, mc 1.20.1, fabric loader 0.14.18 (first version with 1.20 support), and no fabric api
-
 public class EnchantableShears implements ModInitializer {
     private static final String MOD_ID = "enchantableshears";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
